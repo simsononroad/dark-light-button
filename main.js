c4e6443584fd4b56a142a007e77ab3e389f7f48e
simsononroad <d-github@kopaszegy.hu>
2024-12-27 20:44:13 +0100

    shangeable

--- main.js
+++ main.js
@@ -1,34 +1,15 @@
-var isLight = false
-var isDark = false
-var notification = document.getElementById("noti")
+var checkbox = document.getElementById("checkbox")
+var isGrayBackground = 0
 
 function light(){
-    notification.innerText="light"
-    var vilagos = document.getElementById("light")
-    var sotet   = document.getElementById("dark")
-    sotet.style.visibility = "hidden";
-    vilagos.style.visibility = "visible";
-    isLight=true
-    isDark=false
-}
-
-function dark(){
-    notification.innerText="dark"
-    var vilagos = document.getElementById("light")
-    var sotet   = document.getElementById("dark")
-    sotet.style.visibility = "visible";
-    vilagos.style.visibility = "hidden";
-    isDark=true
-    isLight=false
-}
-
-function toggleBtn(){
-    notification.innerText=isLight, ",", isDark
-    if (isDark=="true" && isLight=="false"){
-        
-        light()
-    }if (isLight="true" && isDark=="false"){
-        alert("dark")
-        dark()
+    if (isGrayBackground == 0){
+        var body = document.querySelector("body").style.backgroundColor = "gray"
+        isGrayBackground = 1
+        console.log(isGrayBackground)
+    }else{
+        var body = document.querySelector("body").style.backgroundColor = "white"
+        isGrayBackground = 0
+        console.log(isGrayBackground)
     }
+    
 }
